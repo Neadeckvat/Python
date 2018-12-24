4fd0696696c4a1ad19314b2c6879032baf26c1d9
Neadeckvat <megapoch@gmail.com>
2018-12-25 00:42:50 +0300

    Добавил решение третьей задачи уровня Normal

--- Normal_3.py
+++ Normal_3.py
@@ -6,6 +6,7 @@ __author__ = "Виталий Варщук"
 
 print('\nЗадание-1')
 
+
 def fibonacci(n, m):
     fib = []
     for i in range(n, m + 1):
@@ -23,6 +24,7 @@ def fibonacci(n, m):
             fib.append(b)
     return fib
 
+
 print(fibonacci(1, 11))
 
 # Задача-2:
@@ -31,6 +33,7 @@ print(fibonacci(1, 11))
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 print('\nЗадание-2')
+
 
 def sort_to_max(origin_list):
     count = len(origin_list)-1
@@ -56,19 +59,47 @@ sort_to_max([5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5])
 
 print('\nЗадание-3')
 
+
+def test_func(arg):
+    if arg > 0:
+        return True
+    else:
+        return False
+
+
+def my_filter(func, object):
+    new_object = []
+    if func == None:
+        for n in object:
+            if bool(n) == True:
+                new_object.append(n)
+    else:
+        for n in object:
+            if func(n) == True:
+                new_object.append(n)
+    return new_object
+
+
+array = [2, 0, -12, 2.5, 20, -11, 0, 0, 0, 0]
+print(my_filter(None, array))
+print(my_filter(test_func, array))
+
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
 print('\nЗадание-4')
 
+
 def len_side(point_1, point_2):
     '''Вычисляет длину стороны параллелограмма'''
     return ((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2) ** (0.5)
 
+
 def center_diagonal(point_1, point_2):
     '''Вычисляет точку пересечения диагоналей параллелограмма'''
     return (((point_1[0] + point_2[0]) / 2) , ((point_1[1] + point_2[1]) / 2))
+
 
 def parallelogram(A, B, C, D):
     if len_side(A, B) == len_side(C, D) and len_side(B, C) == len_side(D, A) and center_diagonal(A, C) == center_diagonal(B, D):
