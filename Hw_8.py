02fbfc9549a602a864ebfb22d667dda7fc811409
Neadeckvat <megapoch@gmail.com>
2019-01-24 00:49:27 +0300

    Добавил фильтр ссылок во второй задаче

--- Hw_8.py
+++ Hw_8.py
@@ -6,6 +6,23 @@ __author__ = "Варщук Виталий"
 # 2. Дана строка. Заменить все ссылки и email на ***** (количество звездочек равно длине заменяемого фрагмента).
 # Примеры ссылок: www.site.com, http://site.com и т.п.
 
+
+def check_url(txt, url_lst):
+    txt_lst = txt.split(' ')
+    for n in range(len(txt_lst)):
+        for url in url_lst:
+            if txt_lst[n].find(url) == 0:
+                txt_lst[n] = '*' * len(txt_lst[n])
+    txt_lst = ' '.join(txt_lst)
+    return txt_lst
+
+
+url_list = ['http://', 'https://', 'www.']
+
+text = input("Введите текст: ")
+text = check_url(text, url_list)
+print(text)
+
 # 3. Пользователь бесконечно вводит e-mail адреса. Вывести только те, которые не повторяются.
 
 
