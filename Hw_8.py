3d356d94e9c2a03dfd5ab886f8ccc403d026c4be
Neadeckvat <megapoch@gmail.com>
2019-01-23 22:18:47 +0300

    Добавил решение третьей задачи

--- Hw_8.py
+++ Hw_8.py
@@ -7,3 +7,25 @@ __author__ = "Варщук Виталий"
 # Примеры ссылок: www.site.com, http://site.com и т.п.
 
 # 3. Пользователь бесконечно вводит e-mail адреса. Вывести только те, которые не повторяются.
+
+
+def check_e_mail(e_mail, e_mail_lst, del_lst):
+    if e_mail in del_lst:
+        if e_mail in e_mail_lst:
+            e_mail_lst.remove(e_mail)
+        return e_mail_lst, del_lst
+    else:
+        del_lst.append(e_mail)
+        e_mail_lst.append(e_mail)
+        return e_mail_lst, del_lst
+
+
+e_mail_list = []
+del_list = []
+
+while True:
+    new_e_mail = input("Введите e-mail адрес: ")
+    if "@" not in new_e_mail:
+        continue
+    e_mail_list, del_list = check_e_mail(new_e_mail, e_mail_list, del_list)
+    print(e_mail_list)
